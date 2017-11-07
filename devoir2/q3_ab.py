74dcf2bdf11fd9f16a7a69bab2149e88461e7f29
arthurpe12 <32619888+arthurpe12@users.noreply.github.com>
2017-11-06 21:39:47 -0500

    Update q3_ab.py

--- devoir2/q3_ab.py
+++ devoir2/q3_ab.py
@@ -15,7 +15,7 @@ features1 = features.read().split()
 
 #### partition ######
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify =y, test_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify =y, test_size=0.5, random_state = 42)
 
 
 ###### 3a)  ####################
@@ -27,16 +27,30 @@ xtest_new = chi.transform(X_test)
 word1 = [features1[x] for x in varselect]
 
 
+
+
 kb = SelectKBest(mutual_info_classif,k=10)
 varselect2 = kb.fit(X_train,y_train).get_support(indices=True)
 xtrain_new2 = kb.transform(X_train)
 xtest_new2 = kb.transform(X_test)
 word2 = [features1[x] for x in varselect2]
 
+
 model = LinearSVC()
 score_chi2 = model.fit(xtrain_new,y_train).score(xtest_new,y_test)
 score_mutual = model.fit(xtrain_new2,y_train).score(xtest_new2,y_test)
 score_total = model.fit(X_train,y_train).score(X_test,y_test)
+print('test chi2')
+print(score_chi2)
+print(word1)
+
+print('test mutual')
+print(score_mutual)
+print(word2)
+
+print('test total')
+print(score_total)
+
 
 ######## 3b) ################
 
@@ -46,3 +60,7 @@ xtrain_rfe = RFE.transform(X_train)
 xtest_rfe = RFE.transform(X_test)
 wordrfe = [features1[x] for x in varselect3]
 score_rfe = model.fit(xtrain_rfe,y_train).score(xtest_rfe,y_test)
+print('test_rfe')
+print(score_rfe)
+print(wordrfe)
+
